a51837dcc5d3add3b46d8812bb7fd10a5d56a8f7
s-ishihama-learn <mail@xxx.club.ne.jp>
2020-04-16 08:59:46 +0900

    管理職の追加

--- branch_test.py
+++ branch_test.py
@@ -17,4 +17,18 @@ def trunc10(v):
     return t
 
 
+# 管理職
+def calc_manager(base, post):
+    salary = base + post
+    social = round10(salary * social_rate)
+    tax = (salary - social) * tax_rate
+    bank = salary - social - tax
+    return int(bank)
+
+
 if __name__ == "__main__":
+    bank_terao = calc_manager(350000, 80000)
+    bank_waka = calc_manager(375000, 40000)
+
+    print(f'寺尾哲雄さんの振込額={bank_terao}')
+    print(f'若林仁継さんの振込額={bank_waka}')
