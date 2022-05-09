ba679c2666d6bf5ff3e469a2c5d8c1b26f6371d6
sugawara <sugawara.ken080@gmail.com>
2022-05-09 16:16:39 +0900

    一般社員追加

--- branch_test.py
+++ branch_test.py
@@ -11,4 +11,20 @@ def trunc10(v):
     return t
 
 
+# 一般社員
+def calc_general(base, over):
+ over_price = trunc10(base / 160.0 * 1.25)
+ salary = base + over_price * over
+ social = round10(salary * social_rate)
+ tax = (salary - social) * tax_rate
+ bank = salary - social - tax
+ return int(bank)
+
+
 if __name__ == "__main__":
+ bank_terada = calc_general(320000, 30)
+ bank_hiro = calc_general(295000, 20)
+ bank_suga = calc_general(220000, 35)
+ print(f'寺田帆香さんの振込額={bank_terada}')
+ print(f'広田康博さんの振込額={bank_hiro}')
+ print(f'菅沼洋一郎さんの振込額={bank_suga}')
